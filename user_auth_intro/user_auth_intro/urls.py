2eebc2d667f152fd36860577c28c220cb8398bd0
surajacharya12 <surajacharya.080@kathford.edu.np>
2026-02-05 09:38:55 +0545

    final

--- user_auth_intro/user_auth_intro/urls.py
+++ user_auth_intro/user_auth_intro/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.shortcuts import redirect
+from users import views
+
+def profile_redirect(request):
+    return redirect("dashboard")
 
 urlpatterns = [
-    path("", include("users.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/profile/", profile_redirect),   # 👈 ADD THIS
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("sign_up/", views.sign_up, name="sign_up"),
 ]
--- user_auth_intro/users/urls.py
+++ user_auth_intro/users/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("sign_up/", views.sign_up, name="sign_up"),
-
-
 ]
